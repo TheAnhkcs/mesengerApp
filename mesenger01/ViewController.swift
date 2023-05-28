cd928815eb1e521778435148bf7ae3124d45276b
anhkenzj113 <hoangtheanh89cp@gmail.com>
2023-05-29 00:14:58 +0700

    Initial Commit

---
+++ mesenger01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mesenger01
+//
+//  Created by hoang the anh on 29/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
